dd2160a603c7f4fdef053618466524af20e6b758
Soren-Ding <dingsuren@foxmail.com>
2021-12-24 22:04:32 +0800

    project begin

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,22 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TH_Dec_1' 
+ * Target:  'TH_Dec' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_ADC
+#define RTE_DEVICE_STDPERIPH_DAC
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_I2C
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_DEVICE_STDPERIPH_USART
+
+#endif /* RTE_COMPONENTS_H */
